45e445bbde6fe75c473a6fc610308a498d398815
Eneko Aspillaga <eneko.aspillaga@gmail.com>
2025-06-26 18:12:09 +0200

    Update unwrapDrift to use a kde to estimate the slope

--- R/unwrapDrift.R
+++ R/unwrapDrift.R
@@ -5,10 +5,7 @@
 #' is cyclical (0-1 range), the function unwraps it by correcting for
 #' discontinuities.The correction accounts for the signal emission granularity
 #' of the transmitter, which refers to the precision of the signal emission
-#' intervals. The function estimates the mean slope of the drift using time
-#' differences between consecutive detections. It only considers pairs of
-#' detections with small drift changes (less than half the granularity) and
-#' those occurring within one hour of each other.
+#' intervals.
 #'
 #' @param ms A numeric vector of millisecond values, ranging from 0 to 1.
 #' @param time.stamps A `POSIXct` vector of detection time stamps.
@@ -23,37 +20,37 @@
 #'
 unwrapDrift <- function(ms, time.stamps, granularity = 1) {
 
-  # Create a vector to store the drift
+  # Create a vector to store the unwrapped drift values
   n <- length(ms)
   drift <- numeric(n)
   drift[1] <- ms[1]
 
-  # Calculate differences of drift and time between consecutive detections
+  # Compute differences in drift and time between consecutive detections
   diff_ms <- diff(ms)
   diff_t <- as.numeric(difftime(time.stamps[-1], time.stamps[-n],
                                 units = "secs"))
 
-  # Calculate the mean slope of the time drift, but only taking into account
-  # consecutive detections with small drifts (smaller than half the
-  # granularity). Also, avoid calculating slopes with detections separated by
-  # more than 1 h.
-  indx <- which(abs(diff_ms) < granularity/2 & diff_t < 3600)
-  mean_slope <- mean(diff_ms[indx] / diff_t[indx], na.rm = TRUE, trim = 0.25)
+  # Identify valid detection pairs for slope estimation (consecutive detections
+  # with small drifts - smaller than half the granularity - and detections
+  # separated by less than 1h).
+  indx <- which(abs(diff_ms) < granularity/2 & diff_ms != 0 & diff_t < 3600)
+  dens <- density(diff_ms[indx] / diff_t[indx])
+  slope <- dens$x[which.max(dens$y)]
 
-  # Correct each the drift of each detection based on the mean slope
+  # Reconstruct the continuous drift by correcting wrapped values
   for (i in 2:n) {
 
-    # Expected drift from the previous detection based on the mean slope
-    expected_drift <- drift[i - 1] + diff_t[i - 1] * mean_slope
+    # Predict expected drift based on previous drift and estimated slope
+    expected_drift <- drift[i - 1] + diff_t[i - 1] * slope
 
-    # Calculate the correction factor (n times the granularity)
+    # Compute the correction factor: number of granularity units to shift
     c <- round((expected_drift - ms[i])/granularity)
 
-    # Correct time drift
+    # Apply the correction to unwrap the current drift value
     drift[i] <- ms[i] + c * granularity
   }
 
-  # Set the first drift value to 0 and return drift pattern
+  # Offset the drift so that the first value is zero
   drift <- drift - drift[1]
   return(drift)
 }
